311f51004c38826eb39f8ffa0e86f457924b2446
shubham Rawat <rawatshubam269@gmail.com>
2024-03-13 21:54:13 +0530

    Login Logout Fn using Auth0 added

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,17 +1,35 @@
 import React from "react";
 import { WorkFlowLogo } from "../../assets/logo";
 import "./NavbarStyle.css";
-// import Container from "react-bootstrap/Container";
-// import Nav from "react-bootstrap/Nav";
-// import Navbar from "react-bootstrap/Navbar";
+import { useAuth0 } from "@auth0/auth0-react";
 
 const Navbar = () => {
+  const { user, loginWithRedirect, logout, isAuthenticated } = useAuth0();
   return (
     <div className="navbar__section">
       <WorkFlowLogo />
       <div className="btns">
-        <button>Login</button>
-        <button>Register</button>
+        {isAuthenticated ? (
+          <div className="authentication__info">
+            <div className="user__info">
+              <img
+                className="profile__img"
+                src={user.picture}
+                alt={user.name}
+              />
+              <h2>{user.name}</h2>
+            </div>
+            <button
+              onClick={() =>
+                logout({ logoutParams: { returnTo: window.location.origin } })
+              }
+            >
+              Log Out
+            </button>
+          </div>
+        ) : (
+          <button onClick={() => loginWithRedirect()}>Login</button>
+        )}
       </div>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
-
 import "./css/button.css";
 import "./css/extra.css";
-
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "./pages/Home";
+import ReactDOM from "react-dom/client";
 import WorkFlowPage from "./pages/WorkFlowPage";
+import { Auth0Provider } from "@auth0/auth0-react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 const router = createBrowserRouter([
   {
@@ -24,7 +23,13 @@ const router = createBrowserRouter([
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
+  <Auth0Provider
+    domain="dev-383elx6sz8425dpl.us.auth0.com"
+    clientId="p9Y3doLOjGE5oUN6kCgxAwPwhzWpwgj7"
+    authorizationParams={{
+      redirect_uri: window.location.origin,
+    }}
+  >
     <RouterProvider router={router} />
-  </React.StrictMode>
+  </Auth0Provider>
 );
