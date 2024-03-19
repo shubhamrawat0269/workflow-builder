10ba1af28d52fa6749ec6dcedaad22d3d7c52032
shubham Rawat <rawatshubam269@gmail.com>
2024-03-19 17:52:18 +0530

    Firebase Setup Done

---
+++ src/Firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDwZ9v6uThT7sinelbAqxpoZ7sJvti6FKQ",
+  authDomain: "flowsandbox-11a36.firebaseapp.com",
+  projectId: "flowsandbox-11a36",
+  storageBucket: "flowsandbox-11a36.appspot.com",
+  messagingSenderId: "602956639909",
+  appId: "1:602956639909:web:470c4c96928ca7ffcde74c",
+  measurementId: "G-6569JCT1PF",
+  databaseURL: "https://flowsandbox-11a36-default-rtdb.firebaseio.com",
+};
+
+export const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
--- src/components/SignUpForm/SignUpForm.js
+++ src/components/SignUpForm/SignUpForm.js
@@ -1,6 +1,17 @@
 import React from "react";
+import { app } from "../../Firebase";
+import { getDatabase, ref, set } from "firebase/database";
 
 const SignUpForm = () => {
+  const signInData = (id, name, phone, address) => {
+    // console.log({ id, name, phone, address });
+    const db = getDatabase(app);
+    set(ref(db, "user/" + id), {
+      username: name,
+      phone: phone,
+      address: address,
+    });
+  };
   return (
     <div className="primary-bg w-screen h-screen flex justify-center items-center ">
       <div className="signup-container text-center">
@@ -14,7 +25,12 @@ const SignUpForm = () => {
           Login or register to start building your projects today.
         </p>
         <div className="flex flex-col gap-10">
-          <button className="signup-btn mt-7 w-3/5 m-auto">
+          <button
+            className="signup-btn mt-7 w-3/5 m-auto"
+            onClick={() =>
+              signInData("2", "Aditi", "8375099660", "Shyam Vihar")
+            }
+          >
             Sign in with Github
           </button>
           <button className="btn">Sign in with Google</button>
